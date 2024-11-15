2993a95ebcabe0eaa75ca37d88533dd65bcdaaf8
Ersken <Ersken@DESKTOP-2NV7961>
2024-11-15 17:39:08 +0100

    Add project files.

---
+++ HorseRace/Models/PlayingCard.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace HorseRace.Models
+{
+    public class PlayingCard
+
+    {
+        [Range(2, 13)]
+        public Values value { get; set; }
+        public Suits Suit { get; set; }
+        
+
+
+
+
+    }
+
+
+    /// <summary>
+    /// Suits of Cards
+    /// </summary>
+    public enum Suits
+    {
+        Hearts = 1,
+        Spades = 2,
+        Diamonds = 3,
+        Clubs = 4,
+
+    }
+
+    /// <summary>
+    /// Values of Cards
+    /// </summary>
+    public enum Values
+    {
+        
+        Two = 2,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen, 
+        King,
+
+    }
+
+}
