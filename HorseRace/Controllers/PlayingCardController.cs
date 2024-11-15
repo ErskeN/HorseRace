19435cb05c5ee6228aa456a4c0b4d071bcee954a
Ersken <ericnyman21@outlook.com>
2024-11-15 23:49:09 +0100

    Added players in PlayingCardController.

--- HorseRace/Controllers/PlayingCardController.cs
+++ HorseRace/Controllers/PlayingCardController.cs
@@ -1,11 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//Player.cs (i Models-mappen)
+using HorseRace.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HorseRace.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public Player(string name)
+        {
+            Name = name;
+        }
+    }
+}
+
+//PlayingCardController.cs
 namespace HorseRace.Controllers
 {
     public class PlayingCardController : Controller
     {
+        public Deck CardDeck { get; set; } = new Deck();
+        public List<Player> Players { get; set; } = new List<Player>();
+
         public IActionResult Index()
         {
-            return View();
+            return View(CardDeck);
+        }
+
+        public IActionResult AddPlayer(string playerName)
+        {
+            if (!string.IsNullOrEmpty(playerName))
+            {
+                Players.Add(new Player(playerName));
+                TempData["Message"] = $"Spelare {playerName} har lagts till!";
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult GetPlayers()
+        {
+            return View(Players);
         }
     }
 }
